2dc9a2248d85c45380590f650842250349e591fd
Pedro Kloster <pedroaugustokloster@gmail.com>
2026-06-03 01:52:14 -0300

    Update server.js

--- scripts/server.js
+++ scripts/server.js
@@ -11,7 +11,10 @@ const db = mysql.createConnection({
     user: process.env.DB_USER || 'root',
     password: process.env.DB_PASSWORD || '',
     database: process.env.DB_NAME || 'db_catalogo',
-    port: process.env.DB_PORT || 3306
+    port: process.env.DB_PORT || 4000,
+    ssl:{
+        rejectUnauthorized: true
+    }
 });
 
 db.connect((erro) => {
